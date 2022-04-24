44e47105acbcc5f88287c426b5c02620fb39ad55
heyWFeng <875143930@qq.com>
2022-04-24 14:49:56 +0800

    change config file setup.cfg

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: 程序员晚枫
+# Mail: 1957875073@qq.com
+# Created Time:  2022-4-19 10:17:34
+# Description: https://mp.weixin.qq.com/s/zzD4pxNMFd0ZuWqXlVFdAg
+#############################################
+
+from setuptools import setup            #这个包没有的可以pip一下
+
+setup()
