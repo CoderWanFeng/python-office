a7fcbba4f095a3e58629d42242132ee337e2e9c9
Muspi Merol <wjj123345567@icloud.com>
2022-05-17 10:22:57 +0800

    add PDFReader class to do OCR on PDF

--- office/pdf.py
+++ office/pdf.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
-# from fpdf import FPDF
-# from service.pdf import add_watermark_service
-# import pikepdf
+from fpdf import FPDF
+from service.pdf import add_watermark_service
+import pikepdf
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from functools import cache, cached_property
 
@@ -106,11 +106,26 @@ class PDF:
         return self.doc.page_count
 
     @cache
-    def get_image(self, page=0, dpi=144, alpha=True):
+    def get_pixmap(self, page=0, dpi=108, alpha=True):
         return self.doc[page].get_pixmap(dpi=dpi, alpha=alpha)
 
+    @cache
+    def get_image(self, *args, **kwargs):
+        from imageio import imread
+        from numpy import asarray
+        return asarray(imread(self.get_pixmap(*args, **kwargs).tobytes()))
+
+    @staticmethod
+    def show(image):
+        from matplotlib.pyplot import imshow, show
+        imshow(image)
+        return show()
+
+    def show_image(self, *args, **kwargs):
+        return self.show(self.get_image(*args, **kwargs))
+
     def save_image(self, file_path: str, page=0, dpi=144, alpha=True):
-        return self.get_image(page, dpi, alpha).save(file_path)
+        return self.get_pixmap(page, dpi, alpha).save(file_path)
 
     def save_images(self, dir_path: str, pages=..., dpi=144, alpha=True, encode="png", show_bar=True):
         if not os.path.isdir(dir_path):
@@ -126,6 +141,45 @@ class PDF:
             self.save_image(f"{dir_path}/{page}.{encode}", page, dpi, alpha)
 
 
-if __name__ == '__main__':
-    a = PDF(r"C:\Users\17979\Downloads\ch3 预处理.pdf")
-    a.save_images("output")
+# PDF与OCR
+class PDFReader(PDF):
+    def __init__(self, path: str, lang=("en", "ch_sim")):
+        super().__init__(path)
+        self.lang = lang
+
+    @cached_property
+    def reader(self):
+        from easyocr import Reader
+        return Reader(self.lang)
+
+    @cache
+    def get_texts(self, page=0, *args, **kwargs):
+        return self.reader.readtext(self.get_image(page, *args, **kwargs))
+
+    def render_bbox(self, page=0, *args, color=(255, 0, 0), **kwargs):
+        from cv2 import line
+        image = self.get_image(page, *args, **kwargs).copy()
+        for points, string, degree in self.get_texts(page, *args, **kwargs):
+            c = (*color, round(degree * 255))
+            # noinspection PyPep8Naming
+            A, B, C, D = [tuple(map(round, point)) for point in points]
+            line(image, A, B, c)
+            line(image, B, C, c)
+            line(image, C, D, c)
+            line(image, D, A, c)
+
+        return image
+
+    def show_image_with_bbox(self, *args, **kwargs):
+        return self.show(self.render_bbox(*args, **kwargs))
+
+    def classify(self, choices, page=0, *args, **kwargs):
+        """
+        这个函数的功能在于，比如有一个奖状，上面可能有一等奖二等奖三等奖，要批量识别出一张图片为什么奖
+
+        :param page: the page to ocr
+        :param choices: list or words to choose
+        """
+        from rapidfuzz.process import extractOne
+        texts = [string for _, string, _ in self.get_texts(page, *args, **kwargs)]
+        return sorted((extractOne(choice, texts)[1], choice) for choice in choices)[-1][1]
