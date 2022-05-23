e446bf6d870497871caa5e121bab196bfbc06d6b
CatchDr <49591435+CatchDr@users.noreply.github.com>
2022-05-23 14:19:30 +0800

    删除测试用例 ppt转pptx和pptx转ppt

--- test.py
+++
@@ -1,9 +0,0 @@
-from contributors.CatchDr.doc2docx import doc2docx
-from contributors.CatchDr.docx2doc import docx2doc
-from contributors.CatchDr.ppt2pptx import ppt2pptx
-from  contributors.CatchDr.pptx2ppt import pptx2ppt
-
-# doc2docx("1.doc")
-# docx2doc("1.docx")
-# ppt2pptx("1.ppt")
-pptx2ppt("1.pptx")
