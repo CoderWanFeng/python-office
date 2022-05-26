a8bda3cd6ba32c27392a8d6424b7306dcfa46d71
CatchDr <49591435+CatchDr@users.noreply.github.com>
2022-05-26 12:15:24 +0800

    修复文件夹转换bug

--- test.py
+++
@@ -1,6 +0,0 @@
-# from contributors.CatchDr.pypandoc_tools import docx2pdf
-# docx2pdf(r"D:\python-office\ttt\1908.11415.docx")
-from pypandoc.pandoc_download import download_pandoc
-# # see the documentation how to customize the installation path
-# # but be aware that you then need to include it in the `PATH`
-# download_pandoc()
