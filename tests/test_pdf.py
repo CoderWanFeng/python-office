027ea37a294eadb5d6707a6f740dae5847320d34
程序员晚枫 <1529577833@qq.com>
2022-07-21 01:47:41 +0800

    update

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -11,6 +11,6 @@ class TestExcel(unittest.TestCase):
 
     def test_pdf2docx(self):
         pdf2docx(
-            file_path=r'D:\work\BaiduNetdiskWorkspace\personal\linux\workplace\docs\we-media\自媒体素材\图片\社区\基础入门\Python 入门第一节\0705\常用素材\如何利用Python进行自动化办公.pdf',
-            output_path=r'C:\Users\lenovo\Desktop\package\test\output\test'
+            file_path=r'D:\如何利用Python进行自动化办公.pdf',
+            output_path=r'C:\output\test'
         )
