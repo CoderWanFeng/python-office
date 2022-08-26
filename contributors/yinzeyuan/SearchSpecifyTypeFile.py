dae1de096df06248318e9465ead94f46b36884b4
yinzeyuan <373864470@qq.com>
2022-08-27 04:53:49 +0800

    修改查找指定类型文件功能

--- contributors/yinzeyuan/SearchSpecifyTypeFile.py
+++ contributors/yinzeyuan/SearchSpecifyTypeFile.py
@@ -1,30 +1,36 @@
-import os
 import pathlib
 
 
-def SearchSpecifyTypeFile(Path, FileType: str):
+def search_specify_type_file(file_path, file_type: str):
     '''
 
-    :param Path:目标路径
-    :param FileType:需要查找的文件类型
+    :param file_path:目标路径
+    :param file_type:需要查找的文件类型
     '''
-    Path = pathlib.Path(Path).resolve()
-    if Path.is_dir():
-        FileNameList = list(Path.glob("*"))  # 获取该路径下的文件列表
-        for FileName in FileNameList:
-            FileNameExtension = "".join(list(FileName.suffixes))
-            if FileNameExtension == FileType:
-                print(f"找到了，文件名为{FileName.name}")
+    print("开始查找")
+    i = 0  # 计数变量
+    file_path = pathlib.Path(file_path).resolve()
+    if file_path.is_dir():
+        file_name_list = list(file_path.glob("**/*"))  # 获取该路径下的文件列表
+        for file_name in file_name_list:
+            file_name_extension = "".join(list(file_name.suffixes))
+            if file_name_extension == file_type:
+                print(f"{file_name.name}，{file_name.parent}")
+                i = i + 1
+        print(f"查找完成，共找到{i}个文件")
     else:
         print("请输入文件夹路径")
 
 
 if __name__ == '__main__':
-    SearchSpecifyTypeFile("./test/", ".txt")
-'''
-test目录下有如下文件：'1234tyu', 'ta111test1t.txt', 'taa111test2t.txt', 'taaa111test1t.txt'
+    search_specify_type_file("./test/", ".txt")
+r'''
+test目录下有如下文件：'新建文本文档.txt', '新建文本文档 (2).txt', 并有多层子文件，子文件夹内同样有且仅有上述两个文件
 输出为：
-找到了，文件名为ta111test1t.txt
-找到了，文件名为taa111test2t.txt
-找到了，文件名为taaa111test1t.txt
+新建文本文档 (2).txt，C:\Users\37386\PycharmProjects\Python-officeDev\test
+新建文本文档.txt，C:\Users\37386\PycharmProjects\Python-officeDev\test
+新建文本文档 (2).txt，C:\Users\37386\PycharmProjects\Python-officeDev\test\1
+新建文本文档.txt，C:\Users\37386\PycharmProjects\Python-officeDev\test\1
+新建文本文档 (2).txt，C:\Users\37386\PycharmProjects\Python-officeDev\test\1\2
+新建文本文档.txt，C:\Users\37386\PycharmProjects\Python-officeDev\test\1\2
 '''
