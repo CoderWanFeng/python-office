85b4a5b76a22c26a2d2acb8a0cace52eacce1876
yinzeyuan <373864470@qq.com>
2022-08-31 22:11:43 +0800

    新增检查本地文件夹中的图片是否链接到markdown文件中功能单元测试

--- office/api/markdown.py
+++ office/api/markdown.py
@@ -7,3 +7,8 @@ mainMarkdown = MainMarkdown()
 @except_dec()
 def markdown_link_image_to_base64(markdown_path):
     mainMarkdown.markdown_link_image_to_base64(markdown_path)
+
+
+@except_dec()
+def check_local_dir_image_link_markdown(markdown_path, image_path):
+    mainMarkdown.check_local_dir_image_link_markdown(markdown_path, image_path)
