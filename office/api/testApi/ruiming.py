1b51612bf84a60115d81f1840e4a47a60c3d825c
yinzeyuan <373864470@qq.com>
2022-09-07 15:35:09 +0800

    新增筛选无标签内容的json文件功能（含单元测试）

--- office/api/testApi/ruiming.py
+++ office/api/testApi/ruiming.py
@@ -12,3 +12,8 @@ def screen_unmarked_image(dir_path):
 @except_dec()
 def change_label_in_xml(dir_path, old_label, new_label):
     ruiming.change_label_in_xml(dir_path, old_label, new_label)
+
+
+@except_dec()
+def screen_without_label_json_file(dir_path):
+    ruiming.screen_without_label_json_file(dir_path)
--- tests/test_ruiming.py
+++ tests/test_ruiming.py
@@ -1,6 +1,6 @@
 import unittest
 
-from office.api.testApi.ruiming import screen_unmarked_image, change_label_in_xml
+from office.api.testApi.ruiming import screen_unmarked_image, change_label_in_xml, screen_without_label_json_file
 
 
 class TestExcel(unittest.TestCase):
@@ -9,3 +9,7 @@ class TestExcel(unittest.TestCase):
 
     def test_change_label_in_xml(self):
         change_label_in_xml(dir_path=r".\xml", old_label="测试", new_label="测试1")
+
+    def test_screen_without_label_json_file(self):
+        screen_without_label_json_file(dir_path="./test_files/json")
+        # 预期结果：除1.json外均被移动到”无标签json文件“文件夹中
