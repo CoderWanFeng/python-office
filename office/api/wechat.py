fb3ea21582a5321ccd53e221da6d45cd238bc568
CoderWanFeng <875143930@qq.com>
2022-09-17 21:08:30 +0800

    发布0.2.1👉微信机器人，对话功能

--- office/api/wechat.py
+++ office/api/wechat.py
@@ -1,12 +1,12 @@
-# from office.core.WeChatType import MainWeChat
-#
-# mainwx = MainWeChat()
-
 import PyOfficeRobot
 
 
 def send_message(who, message):
     PyOfficeRobot.chat.send_message(who, message)
+
+
+def chat_by_keywords(who, keywords):
+    PyOfficeRobot.chat.chat_by_keywords(who, keywords)
 
 
 def send_file(who, file):
