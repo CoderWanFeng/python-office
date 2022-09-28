d48adc7a83325c8b328f2a5ea77458ab2b272fa0
CoderWanFeng <1957875073@qq.com>
2022-09-29 00:29:07 +0800

    发布0.2.4👉语音转文字

--- office/api/video.py
+++ office/api/video.py
@@ -2,6 +2,13 @@
 # mainVideo = MainVideo()
 import povideo
 
+
 # 从视频里提取音频
 def video2mp3(path, mp3_name=None):
-    povideo.video2mp3(path,mp3_name)
+    povideo.video2mp3(path, mp3_name)
+
+
+# 从音频里，提取文字
+# 本地语音文件不能大于5MB
+def audio2txt(audio_path, appid, secret_id, secret_key):
+    povideo.audio2txt(audio_path, appid, secret_id, secret_key)
