da665a84a2993ecfb07450eff8570ee0383ae0cf
CoderWanFeng <875143930@qq.com>
2023-07-18 22:05:24 +0800

    add popdf - txt2pdf

---
+++ demo/popdf/TXT转PDF.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+'''
+@作者  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫
+@微信     ：CoderWanFeng : https://mp.weixin.qq.com/s/HYOWV7ImvTXImyYWtwADog
+@个人网站      ：www.python-office.com
+@代码日期    ：2023/7/18 22:01 
+@本段代码的视频说明     ：
+'''
+
+import office
+
+office.pdf.txt2pdf(path=r'./test_files/txt2pdf/程序员晚枫.txt', res_pdf='txt2pdf.popdf', output_path=r'./test_files/txt2pdf/output')
