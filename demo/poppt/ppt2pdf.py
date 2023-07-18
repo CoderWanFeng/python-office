a78e097dcda5a19b74ae44a96d981d20d6c08ccb
CoderWanFeng <875143930@qq.com>
2023-07-18 23:12:03 +0800

    add poppt.ppt2pdf https://mp.weixin.qq.com/s/T31F-U5AdDd3D-61b_K5Qg

---
+++ demo/poppt/ppt2pdf.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+'''
+@作者  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫
+@微信     ：CoderWanFeng : https://mp.weixin.qq.com/s/HYOWV7ImvTXImyYWtwADog
+@个人网站      ：www.python-office.com
+@代码日期    ：2023/7/18 22:45 
+@本段代码的视频说明     ：https://www.bilibili.com/video/BV17Y411c792
+'''
+
+# 导入库：python-office，简写为：office
+import office
+
+# 填入你的ppt目录
+office.ppt.ppt2pdf(path=r'./test_files/ppt2pdf/程序员晚枫.pptx',output_path=r'./test_files/ppt2pdf/output')
