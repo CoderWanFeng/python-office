a13518b9d4b4404b18b18d637930894afa190728
CoderWanFeng <875143930@qq.com>
2023-07-18 22:08:18 +0800

    add popdf - decrypt4pdf

---
+++ demo/popdf/PDF解密.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+'''
+@作者  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫
+@微信     ：CoderWanFeng : https://mp.weixin.qq.com/s/HYOWV7ImvTXImyYWtwADog
+@个人网站      ：www.python-office.com
+@代码日期    ：2023/7/18 22:07 
+@本段代码的视频说明     ：
+'''
+# 导入这个库：python-office，简写为：office
+import office
+
+# PDF解密：填写你的文件位置和密码
+office.pdf.decrypt4pdf(path='你的加密文件.pdf', password='该文件的密码')
