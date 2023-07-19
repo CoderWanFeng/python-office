c2ad0af4dc811c98c4200001df1f701a0edfb978
CoderWanFeng <875143930@qq.com>
2023-07-19 22:49:58 +0800

    add pofile.replace4filename👉https://www.bilibili.com/video/BV12r4y187Yj/

--- demo/pofile/批量重命名.py
+++ demo/pofile/批量重命名.py
@@ -9,4 +9,8 @@
 
 import office
 
-office.file.replace4filename()
+office.file.replace4filename(path=r'./test_files/replace4filename', del_content='程序员晚枫')
+
+import pofile
+
+pofile.replace4filename(path=r'./test_files/replace4filename', del_content='程序员晚枫')
