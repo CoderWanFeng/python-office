74af07ebd0da078357c6ad6078ccac8c6378edf7
CoderWanFeng <875143930@qq.com>
2023-07-20 00:04:44 +0800

    add pofile.group_by_name👉https://mp.weixin.qq.com/s/AVFttFz-IjZD1Ra3K-580A

---
+++ demo/pofile/自动整理文件夹.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+'''
+@作者  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫
+@微信     ：CoderWanFeng : https://mp.weixin.qq.com/s/HYOWV7ImvTXImyYWtwADog
+@个人网站      ：www.python-office.com
+@代码日期    ：2023/7/20 0:04 
+@本段代码的视频说明     ：https://mp.weixin.qq.com/s/AVFttFz-IjZD1Ra3K-580A
+'''
+
+
+import office
+
+path = 'd://程序员晚枫需要整理的文件夹//'
+office.file.group_by_name(path)
