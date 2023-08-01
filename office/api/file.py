d52803a7e26ddc9e1984b8ea0868a26acc69c328
CoderWanFeng <875143930@qq.com>
2023-08-01 21:12:57 +0800

    fix bug : replace4filename

--- office/api/file.py
+++ office/api/file.py
@@ -22,8 +22,19 @@ from office.lib.decorator_utils.instruction_url import instruction
 
 
 @instruction
-def replace4filename(path: str, del_content, replace_content=None):
-    pofile.replace4filename(path, del_content, replace_content)
+def replace4filename(path: str, del_content, replace_content='', dir_rename: bool = True,
+                     file_rename: bool = True, suffix=None):
+    """
+    批量修改文件/文件夹名称
+    :param path: 必填，需要修改文件夹/文件名称的根目录，注意：该根目录名称不会被修改
+    :param del_content: 必填，需要替换/删除的内容
+    :param replace_content: 选填，替换后的内容，不填则实现删除效果
+    :param dir_rename: 选填，是否修改文件夹名称，默认：修改
+    :param file_rename: 选填，是否修改文件名称，默认：修改
+    :param suffix: 选填，指定修改的文件类型，默认：所有
+    :return:
+    """
+    pofile.replace4filename(path, del_content, replace_content, dir_rename, file_rename, suffix)
 
 
 # todo：输入文件路径
