ee52a39eb8e299734bca7c6e979ba7d91a95bc63
CoderWanFeng <875143930@qq.com>
2023-08-09 22:57:45 +0800

    add 文本转词云

---
+++ demo/poimage/文本转词云.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+'''
+@作者  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫
+@微信     ：CoderWanFeng : https://mp.weixin.qq.com/s/HYOWV7ImvTXImyYWtwADog
+@个人网站      ：www.python-office.com
+@代码日期    ：2023/8/9 22:48 
+@本段代码的视频说明     ：
+'''
+import poimage
+
+poimage.txt2wordcloud()
