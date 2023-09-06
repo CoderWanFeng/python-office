4e9b615fdc43f8d71a5bb8563be0cc2649510d42
CoderWanFeng <875143930@qq.com>
2023-09-06 23:21:17 +0800

    发布：文字转语音👉https://juejin.cn/post/7275589957158879288

--- office/api/video.py
+++ office/api/video.py
@@ -2,9 +2,9 @@
 # mainVideo = MainVideo()
 import povideo
 
-
 # 从视频里提取音频
 from office.lib.decorator_utils.instruction_url import instruction
+
 
 @instruction
 def video2mp3(path, mp3_name=None, output_path=r'./'):
@@ -16,6 +16,7 @@ def video2mp3(path, mp3_name=None, output_path=r'./'):
 @instruction
 def audio2txt(audio_path, appid, secret_id, secret_key):
     povideo.audio2txt(audio_path, appid, secret_id, secret_key)
+
 
 @instruction
 def mark2video(video_path, output_path=r'./', output_name=r'mark2video.mp4', mark_str: str = "www.python-office.com",
@@ -33,3 +34,18 @@ def mark2video(video_path, output_path=r'./', output_name=r'mark2video.mp4', mar
     :return:
     """
     povideo.mark2video(video_path, output_path, output_name, mark_str, font_size, font_type, font_color)
+
+
+def txt2mp3(content='程序员晚枫', file=None, mp3=r'./程序员晚枫.mp3', speak=True):
+    """
+    文本转语音
+    Args:
+        content:需要转换的内容
+        file: 指定读取的文件，优先级最高
+        mp3: 需要保存的mp3位置和名称。填None不保存。
+        speak:是否阅读
+
+    Returns:
+
+    """
+    return povideo.txt2mp3(content, file, mp3, speak)
