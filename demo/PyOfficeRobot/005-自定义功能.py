cf5ae09ed8c5cfc93a9d02681ead0a73ae86dc18
CoderWanFeng <875143930@qq.com>
2023-11-23 23:27:32 +0800

    发布：0.4.21👉comment excel2pdf

--- demo/PyOfficeRobot/005-自定义功能.py
+++ demo/PyOfficeRobot/005-自定义功能.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 '''
 @作者  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫
-@微信     ：CoderWanFeng : https://mp.weixin.qq.com/s/B1V6KeXc7IOEB8DgXLWv3g
+@微信     ：CoderWanFeng : https://mp.weixin.qq.com/s/Nt8E8vC-ZsoN1McTOYbY2g
 @个人网站      ：www.python-office.com
 @代码日期    ：2023/7/9 23:22 
 @本段代码的视频说明     ：https://www.bilibili.com/video/BV14R4y127h6
--- demo/PyOfficeRobot/010-定时群发.py
+++ demo/PyOfficeRobot/010-定时群发.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 '''
 @作者 ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫
-@微信 ：CoderWanFeng : https://mp.weixin.qq.com/s/B1V6KeXc7IOEB8DgXLWv3g
+@微信 ：CoderWanFeng : https://mp.weixin.qq.com/s/Nt8E8vC-ZsoN1McTOYbY2g
 @个人网站 ：www.python-office.com
 @Date    ：2023/4/27 21:20 
 @Description     ：
