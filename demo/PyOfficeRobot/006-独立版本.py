4a1a3f940203fe2b7d7c7a780022d64638196301
CoderWanFeng <1957875073@qq.com>
2024-03-17 06:17:37 +0800

    更新简介

--- demo/PyOfficeRobot/006-独立版本.py
+++ demo/PyOfficeRobot/006-独立版本.py
@@ -1,8 +1,8 @@
 # -*- coding: UTF-8 -*-
 '''
-@作者  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫
-@微信     ：CoderWanFeng : https://mp.weixin.qq.com/s/Nt8E8vC-ZsoN1McTOYbY2g
-@个人网站      ：www.python-office.com
+@作者  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫，微信：CoderWanFeng
+@读者群     ：http://www.python4office.cn/wechat-group/
+@学习网站      ：www.python-office.com
 @代码日期    ：2023/7/9 23:25 
 @本段代码的视频说明     ：https://www.bilibili.com/video/BV1SY411y7Uh
 '''
