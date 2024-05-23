4679dbf16a4414484e220b74f13ed30a01d0c4ca
CoderWanFeng <1957875073@qq.com>
2024-05-23 23:20:37 +0800

    更新网站：https://www.python-office.com

--- demo/PyOfficeRobot/003-根据关键词回复.py
+++ demo/PyOfficeRobot/003-根据关键词回复.py
@@ -2,7 +2,7 @@
 '''
 @作者  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫，微信：CoderWanFeng
 @读者群     ：http://www.python4office.cn/wechat-group/
-@学习网站      ：www.python-office.com
+@学习网站      ：https://www.python-office.com
 @Date    ：2023/2/13 21:19
 @本段代码的视频说明     ：https://www.bilibili.com/video/BV1m8411b7LZ
 '''
--- demo/PyOfficeRobot/011-chat_chatgpt.py
+++ demo/PyOfficeRobot/011-chat_chatgpt.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 '''
 @作者 ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫
-@微信 ：CoderWanFeng : https://mp.weixin.qq.com/s/Nt8E8vC-ZsoN1McTOYbY2g
+@微信 ：CoderWanFeng : https://mp.weixin.qq.com/s/8x7c9qiAneTsDJq9JnWLgA
 @个人网站 ：www.python-office.com
 @Date    ：2023/3/19 18:17 
 @Description     ：
