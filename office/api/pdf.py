11097215799f2eef22e28a5dd5ede028c418f4b1
CoderWanFeng <1957875073@qq.com>
2024-12-09 23:12:53 +0800

    测试：popdf的1.0.0版本

--- office/api/pdf.py
+++ office/api/pdf.py
@@ -10,7 +10,84 @@
 # @except_dec()
 import popdf
 
-from office.lib.decorator_utils.instruction_url import instruction
+
+# todo：输入文件路径
+# @except_dec()
+def pdf2docx(input_file, output_path='.'):
+    """
+    PDF转Word
+    文档：http://www.python4office.cn/python-office/popdf/%E8%AF%BE%E7%A8%8B/1-pdf2docx/
+    """
+    popdf.pdf2docx(input_file, output_path)
+
+
+# @except_dec()
+def pdf2imgs(input_file, output_path, merge=False):
+    """
+    pdf转图片
+    文档：http://www.python4office.cn/python-office/popdf/%E8%AF%BE%E7%A8%8B/2-pdf2imgs/
+    """
+    popdf.pdf2imgs(input_file, output_path, merge)
+
+
+def txt2pdf(input_file: str, output_file='txt2pdf.pdf'):
+    """
+    将文本文件转换为PDF文件。
+    文档：http://www.python4office.cn/python-office/popdf/%E8%AF%BE%E7%A8%8B/3-txt2pdf/
+    """
+
+    popdf.txt2pdf(input_file, output_file)
+
+
+def split4pdf(input_file, output_file=r'./output_path/split_pdf.pdf', from_page=-1, to_page=-1):
+    """
+    拆分PDF文件
+    文档：http://www.python4office.cn/python-office/popdf/%E8%AF%BE%E7%A8%8B/4-split4pdf/
+    """
+    popdf.split4pdf(input_file, output_file, from_page, to_page)
+
+
+def encrypt4pdf(input_file, password, output_file):
+    """
+    加密pdf
+    文档：http://www.python4office.cn/python-office/popdf/%E8%AF%BE%E7%A8%8B/5-encrypt4pdf/
+    演示代码：
+    """
+    popdf.encrypt4pdf(input_file, password, output_file)
+
+
+def decrypt4pdf(input_file, password, output_file='decrypt.pdf'):
+    """
+    解密pdf
+    文档：http://www.python4office.cn/python-office/popdf/%E8%AF%BE%E7%A8%8B/6-decrypt4pdf
+    演示代码：
+    """
+    popdf.decrypt4pdf(input_file, password, output_file)
+
+
+def add_text_watermark(input_file, point, text='python-office',
+                       output_file='./pdf_watermark.pdf', fontname="Helvetica", fontsize=12, color=(1, 0, 0)) -> None:
+    """
+    在PDF文档中添加文本水印。
+    文档：http://www.python4office.cn/python-office/popdf/%E8%AF%BE%E7%A8%8B/7-add_watermark
+    """
+    popdf.add_watermark(input_file, point, text,
+                        output_file, fontname, fontsize, color)
+
+
+def merge2pdf(input_file_list, output_file):
+    """
+    合并pdf
+    文档：http://www.python4office.cn/python-office/popdf/%E8%AF%BE%E7%A8%8B/8-merge2pdf
+    演示代码：
+    """
+    popdf.merge2pdf(input_file_list, output_file)
+
+
+# @except_dec()
+# @instruction
+def add_img_water(pdf_file_in, pdf_file_mark, pdf_file_out):
+    popdf.add_img_water(pdf_file_in, pdf_file_mark, pdf_file_out)
 
 
 # @instruction
@@ -48,60 +125,3 @@ def add_watermark_by_parameters(pdf_file, mark_str, output_path=None, output_fil
 
     """
     popdf.add_watermark_by_parameters(pdf_file, mark_str, output_path, output_file_name)
-
-
-# txt转pdf
-# @except_dec()
-# @instruction
-def txt2pdf(path: str, res_pdf='txt2pdf.popdf', output_path=r'./'):
-    popdf.txt2pdf(path, res_pdf, output_path)
-
-
-# PDF加密
-# @except_dec()
-# @instruction
-def encrypt4pdf(path, password, output_path=None):
-    popdf.encrypt4pdf(path, password, output_path)
-
-
-# PDF解密
-# @except_dec()
-# @instruction
-def decrypt4pdf(path, password, res_pdf='decrypt.popdf'):
-    popdf.decrypt4pdf(path, password, res_pdf)
-
-
-# 合并pdf
-# @except_dec()
-# @instruction
-def merge2pdf(one_by_one, output):
-    popdf.merge2pdf(one_by_one, output)
-
-
-# todo：输入文件路径
-# @except_dec()
-# @instruction
-def pdf2docx(file_path: str, output_path: str = './'):
-    """
-    PDF转Word
-    视频：https://www.bilibili.com/video/BV1em4y1H7ir/
-    Args:
-        file_path: pdf的存储位置。批量处理：只填写文件夹就行
-        output_path: 转换后的输出位置
-
-    Returns:
-
-    """
-    popdf.pdf2docx(file_path, output_path)
-
-
-# @except_dec()
-# @instruction
-def pdf2imgs(pdf_path, out_dir, merge=False):
-    popdf.pdf2imgs(pdf_path, out_dir, merge)
-
-
-# @except_dec()
-# @instruction
-def add_img_water(pdf_file_in, pdf_file_mark, pdf_file_out):
-    popdf.add_img_water(pdf_file_in, pdf_file_mark, pdf_file_out)
