61830446272d79c6ef063bf497cfda41783c332d
Administrator <1639972003@qq.com>
2025-06-06 12:17:48 +0800

    微信机器人中添加注释

--- examples/PyOfficeRobot/003-根据关键词回复.py
+++ examples/PyOfficeRobot/003-根据关键词回复.py
@@ -8,5 +8,6 @@ keywords = {
     "关注了吗？": "必须的",
     "投币了吗？": "三连走起",
 }
+# keywords 中 前面一个引号里的内容是好友发来的消息，后面一个引号里的内容是回复给好友的消息
 PyOfficeRobot.chat.chat_by_keywords(who='抖音：程序员晚枫', keywords=keywords)
 # PyOfficeRobot.chat.chat_by_keywords(who='每天进步一点点', keywords=keywords)
