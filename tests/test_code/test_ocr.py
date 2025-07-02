6fb8db5c0658b2483380487b6765fe937722576c
CoderWanFeng <1957875073@qq.com>
2025-07-02 23:14:13 +0800

    发布1.0.1：增加发票识别功能

---
+++ tests/test_code/test_ocr.py
@@ -0,0 +1,26 @@
+# -*- coding: UTF-8 -*-
+'''
+@学习网站      ：https://www.python-office.com
+@读者群     ：http://www.python4office.cn/wechat-group/
+@作者  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫，微信：CoderWanFeng
+@代码日期    ：2025/7/2 22:45 
+@本段代码的视频说明     ：
+'''
+import os
+import unittest
+
+from loguru import logger
+
+from office.api.ocr import VatInvoiceOCR2Excel
+
+
+class TestPDF(unittest.TestCase):
+    def setUp(self):
+        self.id = os.getenv("SecretId", None)
+        self.key = os.getenv("SecretKey", None)
+
+    def test_VatInvoiceOCR2Excel(self):
+        logger.info(self.id)
+        logger.info(self.key)
+        VatInvoiceOCR2Excel(input_path=r'..\test_files\ocr\img.png', output_path=r'./output/ocr',
+                            id=self.id, key=self.key)
