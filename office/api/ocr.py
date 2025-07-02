e57a37523b5a98e6faf9175fb09564919aa5ee31
CoderWanFeng <1957875073@qq.com>
2025-07-02 23:46:55 +0800

    修改了文件夹结构

--- office/api/ocr.py
+++ office/api/ocr.py
@@ -5,6 +5,21 @@ import poocr
 
 def VatInvoiceOCR2Excel(input_path, output_path=r'./', output_excel='VatInvoiceOCR2Excel.xlsx', img_url=None,
                         id=None, key=None, file_name=False, trans=False):
+    """
+    发票识别
+    Args:
+        input_path:
+        output_path:
+        output_excel:
+        img_url:
+        id:
+        key:
+        file_name:
+        trans:
+
+    Returns:
+
+    """
     poocr.ocr2excel.VatInvoiceOCR2Excel(input_path=input_path, output_path=output_path,
                                         output_excel=output_excel,
                                         img_url=img_url,
