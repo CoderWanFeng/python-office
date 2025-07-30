f849837230bb31369d494c35804e4bc73e829996
laonong-1024 <kylnwu@qq.com>
2025-07-30 16:26:44 +0800

    update: 更新文件 002-发文件.py 完善注释
    Signed-off-by: laonong-1024 <kylnwu@qq.com>

--- examples/PyOfficeRobot/002-发文件.py
+++ examples/PyOfficeRobot/002-发文件.py
@@ -3,5 +3,5 @@
 import PyOfficeRobot
 
 # PyOfficeRobot.file.send_file(who='每天进步一点点', file=r'C:\Users\Lenovo\Desktop\temp\0.jpg')
-# who 后面写的微信好友的昵称/备注名，file是文件路径
+# who 后面写的微信好友的昵称/备注名，file是文件路径，win系统的话，文件路径如果用“\”，前面一定要加上"r"，如下：
 PyOfficeRobot.file.send_file(who='B站：程序员晚枫', file=r'C:\Users\Lenovo\Desktop\temp\0.jpg')
