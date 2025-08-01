0b574e5380483f871d4665649ff04cd6f14e83ec
laonong-1024 <kylnwu@qq.com>
2025-08-01 11:01:48 +0800

    update: 更新文件 006-独立版本.py
    Signed-off-by: laonong-1024 <kylnwu@qq.com>

--- examples/PyOfficeRobot/006-独立版本.py
+++ examples/PyOfficeRobot/006-独立版本.py
@@ -1,14 +1,14 @@
 # -*- coding: UTF-8 -*-
-
+# Author: 程序员晚枫
 
 # 原始方式
-import office
+# import office
 
-office.wechat.send_message(who='百度一下：程序员晚枫', message='点个star吧')
+# office.wechat.send_message(who='百度一下：程序员晚枫', message='点个star吧')
 # office.wechat.send_message(who='每天进步一点点', message='你好')
 
 # 独立方式
-# import PyOfficeRobot
+import PyOfficeRobot
 
 
-# PyOfficeRobot.chat.send_message(who='百度一下：程序员晚枫', message='点个star吧')
+PyOfficeRobot.chat.send_message(who='百度一下：程序员晚枫', message='点个star吧')
