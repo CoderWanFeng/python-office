fcc74f8eecc3c908e3bf58480d2740fc3b1b33a0
rs1973-bit <dingzheng_2023@qq.com>
2025-11-05 21:08:05 +0800

    提交批量处理图片的代码

--- contributors/rs1973/process_img1.py
+++ contributors/rs1973/process_img1.py
@@ -1,5 +1,5 @@
 """"
-Creator:rs1973-bit
+Creator:rs1973
 E-mail:dingzheng_2023@qq.com/gunshi98@gmail.com
 Description:支持输入一个目录或一张图片,修改它(或者整个目录里)的图片尺寸,色彩通道,并决定是否留下图像的
 alpha值,会自动筛选不是图片或不支持的格式 程序为多线程(多进程下使用笔记本测试时cpu过热),但gif合成
@@ -11,10 +11,7 @@ ps:这是我的第一个项目,有很多做得不够好的地方,请多包容
 import os
 import time
 from concurrent.futures import ThreadPoolExecutor
-
-
 from PIL import Image, ImageOps
-
 
 def get_img(srcpath=None, sinfile=None):
     """筛选目录中的非图片文件,只筛选一层,不读取子目录里的文件"""
@@ -112,9 +109,10 @@ def process_img(outpath=None, input_info=None, kind=None, alpha=None, img=None):
 
 
 def main(srcpath: str = None, outpath: str = None, sinfile: str = None,
-         input_info: tuple = None, kind: str = '.jpeg', alpha: bool = False,
-         duration: int = 300, process: int = 5):
+         img_size: tuple = None, kind: str = '.jpeg', alpha: bool = False,
+         duration: int = 300, process: int = int(os.cpu_count()//2), name='index'):
     """主逻辑函数"""
+    print('开始处理图片……')
     start = time.time()
 
     filenames = get_img(srcpath, sinfile)
@@ -123,13 +121,19 @@ def main(srcpath: str = None, outpath: str = None, sinfile: str = None,
 
     if kind == '.gif':
         print('注意:请确保图像列表中所有图片的大小都一样')
-        gif(filenames, outpath, duration)
+        gif(filenames, outpath, duration, name)
     else:
         with ThreadPoolExecutor(process) as pool:
             futures = [pool.submit(process_img, outpath,
-                                   input_info, kind, alpha, img) for img in filenames]
+                                   img_size, kind, alpha, img) for img in filenames]
             for fut in futures:
                 fut.result()
 
     end = time.time()
     print(f'处理完成, 耗时: {end - start:.2f}s')
+
+# src = r"C:\Users\rollingstone\OneDrive\Desktop\001"
+# out = r"C:\Users\rollingstone\OneDrive\Desktop\s\re"
+
+# if __name__ == '__main__':
+#     main(srcpath=src, outpath=out, alpha=False, kind='.gif', name='hello', duration=250)
